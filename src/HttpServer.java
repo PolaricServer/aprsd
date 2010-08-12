39377c25e5431f1e8e05454b07b1ee31705d1371
la7eca <la7eca@f49ff3f0-531d-43b2-bf2a-0429c094f1ea>
2010-08-12 22:19:28 +0000

    Many fixes

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -104,21 +104,35 @@ public abstract class HttpServer extends NanoHTTPD
 
        if (user == null)
           user="_NOLOGIN_";
-       return ( user.equals(_adminuser) ||
+       return ( user.matches(_adminuser) ||
                 user.matches(_updateusers) );
    }
-
-
    
+
+   protected final boolean authorizedForAdmin(Properties header)
+   {
+       String user = getAuthUser(header);
+       return (user != null && user.matches(_adminuser)); 
+   } 
+   
+   
+   
+   protected int _requests = 0, _reqNo = 0;
 
    /** 
     * Generic HTTP serve method. Dispatches to other methods based on uri
     */
    public Response serve( String uri, String method, Properties header, Properties parms )
    {
+       int reqNo;
+       synchronized (this) {
+         _requests++; _reqNo++;
+         reqNo = _reqNo;
+       }
+       
        try {
          String type; 
-         ByteArrayOutputStream os = new ByteArrayOutputStream(1024);
+         ByteArrayOutputStream os = new ByteArrayOutputStream(2048);
          PrintWriter out = new PrintWriter(new OutputStreamWriter(os, _encoding));
          
          /* Determine the View Filter. Currently, there are just two
@@ -127,8 +141,10 @@ public abstract class HttpServer extends NanoHTTPD
           */
          String filtid = _infraonly ? "infra" : parms.getProperty("filter");
          ViewFilter vfilt = ViewFilter.getFilter(filtid);
-         
-         if ("/status".equals(uri))
+       //  System.out.println("HTTP REQ ["+reqNo+"]: "+uri+", "+parms);
+         if ("/admin".equals(uri))
+             type = _serveAdmin(header, parms, out);   
+         else if ("/status".equals(uri))
              type = _serveStatus(header, parms, out, vfilt);
          else if ("/station".equals(uri))
              type = _serveStation(header, parms, out);
@@ -142,11 +158,10 @@ public abstract class HttpServer extends NanoHTTPD
              type = _serveAddObject(header, parms, out);
          else if ("/deleteobject".equals(uri))
              type = _serveDeleteObject(header, parms, out);
-         else if ("/deleteallobj".equals(uri))
-             type = _serveDeleteAllObjects(header, parms, out);
+         else if ("/resetinfo".equals(uri))
+             type = _serveResetInfo(header, parms, out);    
          else 
              return serveFile( uri, header, new File("."), true );
-       
        
          InputStream is = new ByteArrayInputStream(os.toByteArray());   // FIXME: this copies buffer content       
          Response res = new Response(HTTP_OK, type, is);
@@ -154,12 +169,14 @@ public abstract class HttpServer extends NanoHTTPD
          return res; 
        }
        catch (IOException e) {return null;} 
+       finally { _requests--; }
    }
 
+   protected abstract String _serveAdmin(Properties header, Properties parms, PrintWriter out);
    protected abstract String _serveStation(Properties header, Properties parms, PrintWriter out);
    protected abstract String _serveAddObject (Properties header, Properties parms, PrintWriter out);
    protected abstract String _serveDeleteObject (Properties header, Properties parms, PrintWriter out);
-   protected abstract String _serveDeleteAllObjects (Properties header, Properties parms, PrintWriter out);
+   protected abstract String _serveResetInfo (Properties header, Properties parms, PrintWriter out);
    protected abstract String _serveStatus (Properties header, Properties parms, PrintWriter out, ViewFilter vf);
    protected abstract String _serveStationHistory (Properties header, Properties parms, PrintWriter out);
 
@@ -183,7 +200,7 @@ public abstract class HttpServer extends NanoHTTPD
     */
    public String serveFindStation(Properties header, Properties parms, PrintWriter out)
    { 
-       Station s = _db.getStation(parms.getProperty("id"));
+       Station s = _db.getStation(parms.getProperty("id").toUpperCase());
        if (s!=null && !s.expired() && s.getPosition() != null) {
           UTMRef xpos = toUTM(s.getPosition()); 
           out.println(s.getIdent()+","+ (long) Math.round(xpos.getEasting()) + "," + (long) Math.round(xpos.getNorthing()));   
@@ -232,12 +249,17 @@ public abstract class HttpServer extends NanoHTTPD
         }
         
         /* Sequence number at the time of request */
-        _seq = (_seq+1) % 32000;
+        long seq  = 0;
+        synchronized (this) {
+          _seq = (_seq+1) % 32000;
+          seq = _seq;
+        }
         
+        /* If requested, wait for a state change (see Notifier.java) */
         if (parms.getProperty("wait") != null) 
             Station.waitChange(uleft, lright);
-            
-        synchronized (this) {    
+                
+        /* XML header with meta information */           
         out.println("<overlay seq=\""+_seq+"\"" +
             (filt==null ? ""  : " view=\"" + filt + "\"") + ">");
         out.println("<meta name=\"utmzone\" value=\""+ _utmzone + "\"/>");
@@ -245,6 +267,10 @@ public abstract class HttpServer extends NanoHTTPD
         out.println("<meta name=\"adminuser\" value=\""+ _adminuser.equals(getAuthUser(header)) + "\"/>");
         out.println("<meta name=\"updateuser\" value=\""+ authorizedForUpdate(header) + "\"/>");
             
+            
+        /* Output signs. A sign is not an APRS object
+         * just a small icon and a title. This will probably be removed. 
+         */
         int i=0;
         for (Signs.Item s: Signs.getList())
         {
@@ -260,6 +286,7 @@ public abstract class HttpServer extends NanoHTTPD
             out.println("</point>");    
         }        
         
+        /* Output APRS objects */
         for (AprsPoint s: _db.search(uleft, lright)) 
         {
             synchronized (s) 
@@ -273,13 +300,13 @@ public abstract class HttpServer extends NanoHTTPD
                if (ref == null) continue; 
             
                if (!s.visible()) 
-                   out.println("<delete id=\""+s.getIdent()+"\"/>");
+                   out.println("<delete id=\""+fixText(s.getIdent())+"\"/>");
                else {
                   String title = s.getDescr() == null ? "" 
-                             : "title=\"[" + s.getIdent() + "] " + fixText(s.getDescr()) + "\"";
+                             : "title=\"[" + fixText(s.getIdent()) + "] " + fixText(s.getDescr()) + "\"";
                   String icon = "srv/icons/"+ (s.getIcon() != null ? s.getIcon() : _icon);    
                 
-                  out.println("<point id=\""+s.getIdent()+"\" x=\""
+                  out.println("<point id=\""+fixText(s.getIdent())+"\" x=\""
                                + (int) Math.round(ref.getEasting()) + "\" y=\"" + (int) Math.round(ref.getNorthing())+ "\" " 
                                + title + (s.isChanging() ? " redraw=\"true\"" : "") +
                                ((s instanceof AprsObject) && Main.ownobjects.hasObject(s.getIdent())  ? " own=\"true\"":"") +">");
@@ -300,7 +327,7 @@ public abstract class HttpServer extends NanoHTTPD
                      }
                                                    
                      out.println("   <label style=\""+style+"\">");
-                     out.println("       "+s.getDisplayId());
+                     out.println("       "+fixText(s.getDisplayId()));
                      out.println("   </label>"); 
                   }
                   
@@ -313,14 +340,18 @@ public abstract class HttpServer extends NanoHTTPD
                   out.println("</point>");
                }
             }
+            /* Allow other threads to run */ 
+            Thread.currentThread ().yield ();
         }        
         out.println("</overlay>");
-       }
         out.flush();
         return "text/xml; charset=utf-8";
    }
 
      
+   /**
+    * Display a message path between nodes. 
+    */
    private void printPathXml(PrintWriter out, Station s, UTMRef uleft, UTMRef lright)
    {
        UTMRef ity = toUTM(s.getPosition());
