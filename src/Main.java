8d5fc376d525df9cdf0ddbd284cb1212055ab3e9
la7eca <la7eca@f49ff3f0-531d-43b2-bf2a-0429c094f1ea>
2010-08-21 00:52:07 +0000

    version id

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import java.nio.charset.Charset;
 
 public class Main
 {
-   public static String version = "1.0b2+test1";
+   public static String version = "1.0b3";
    static StationDB db = null;
    public static InetChannel ch1 = null;
    public static TncChannel  ch2 = null;
