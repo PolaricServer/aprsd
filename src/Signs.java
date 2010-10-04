8014c50c3107facb5509b68706024f6dcbec8a11
la7eca <la7eca@f49ff3f0-531d-43b2-bf2a-0429c094f1ea>
2010-10-04 21:55:34 +0000

    Improvements...

--- src/Signs.java
+++ src/Signs.java
@@ -9,19 +9,27 @@ public class Signs
 {
     private static Signs _signs = new Signs("signs");
     
-    public static class Item {
-        public Reference pos;
-        public String icon;
-        public String text;
-        public Item (Reference r, String ic, String txt)
-          { pos = r; icon = ic; text = txt; }
+    public static class Item extends Point {
+        public long _maxScale;
+        
+        public boolean visible(long scale)
+          { return scale <= _maxScale; }
+          
+        public Item (Reference r, long sc, String ic, String txt)
+          { _position = r; _maxScale = sc; _icon = ic; _description = txt; }
     }
     
     private BufferedReader  _rd;
     private StringTokenizer _next;
     private List<Item> _list = new ArrayList();
   
-    
+    /**
+     * Read signs file.
+     * Format of each line: 
+     * UTM-zone, UTM-easting, UTM-northing, max-scale, icon-filename, description.
+     * Example:  
+     *    33Wi, 123456i, 1234567, 30000, symbol.gif, This is my site 
+     */
     protected Signs(String file) 
     {
         try {
@@ -32,17 +40,19 @@ public class Signs
                if (!line.startsWith("#")) 
                {                 
                    String[] x = line.split(",\\s*");  
-                   if (x.length < 5)
+                   if (x.length < 6)
                        continue;
                    if (!x[0].matches("[0-9]{2}[a-zA-Z]") || 
-                       !x[1].matches("[0-9]{6}") || !x[2].matches("[0-9]{7}"))
+                       !x[1].matches("[0-9]{6}") || !x[2].matches("[0-9]{7}") ||
+                       !x[3].matches("[0-9]+"))
                        continue;    
                    double easting = Long.parseLong(x[1]);
                    double northing = Long.parseLong(x[2]);
+                   long scale = Long.parseLong(x[3]);
                    Reference pos = new UTMRef( Integer.parseInt( x[0].substring(0,2)), 
                                                x[0].charAt(2),
                                                easting, northing);
-                   Item it = new Item(pos, x[3], x[4]);
+                   Item it = new Item(pos, scale, x[4], x[5]);
                    _list.add(it);
                }
             }     
