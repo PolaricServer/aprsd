b53177aa6237bcb78fa2af1c89ae20dc05479211
la7eca <la7eca@f49ff3f0-531d-43b2-bf2a-0429c094f1ea>
2011-10-01 22:04:36 +0000

    PosData as separate class

--- src/AprsHandler.java
+++ src/AprsHandler.java
@@ -11,8 +11,23 @@ import uk.me.jstott.jcoord.*;
  
 public interface AprsHandler
 {
-    public void handlePosReport(String sender, Date ts, Reference newpos, int crs, int sp, int alt, 
-            String descr, char sym, char altsym, String pathinfo);
+
+    public static class PosData {
+       public Reference pos;
+       public int course = -1;
+       public int speed = -1;
+       public char symbol, symtab;
+       public long altitude = -1; 
+       public PosData () {}
+       public PosData (Reference p, char sym, char stab)
+          {pos=p; symbol=sym; symtab=stab; }
+       public PosData (Reference p, int crs, int sp, char sym, char stab)
+          {pos=p; course=crs; speed=sp; symbol=sym; symtab=stab; }   
+    }
+
+
+    public void handlePosReport(String sender, Date ts, PosData newpos,  
+            String descr, String pathinfo);
     
     public void handleStatus(Date ts, String msg, String pathinfo);
                                    
@@ -26,8 +41,8 @@ public interface AprsHandler
      */
     public class Dummy implements AprsHandler 
     {
-       public void handlePosReport(String sender, Date ts, Reference newpos, int crs, int sp, int alt, 
-               String descr, char sym, char altsym, String pathinfo) {};
+       public void handlePosReport(String sender, Date ts, PosData newpos,  
+               String descr, String pathinfo) {};
        public void handleStatus(Date ts, String msg, String pathinfo) {};                               
        public void handleMessage(Date ts, String src, String dest, String msg) {}
        public void handlePacket(Date ts, String src, String dest, String path, String txt) {}
