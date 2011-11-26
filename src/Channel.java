955221a3c65d6cb7a2b41edfb8dadefa3f848404
la7eca <la7eca@f49ff3f0-531d-43b2-bf2a-0429c094f1ea>
2011-11-26 23:59:45 +0000

    Support registering path for heard packets. Reverse digipath when sending messages, etc...

--- src/Channel.java
+++ src/Channel.java
@@ -1,6 +1,6 @@
  
 /* 
- * Copyright (C) 2009 by LA7ECA, Øyvind Hanssen (ohanssen@acm.org)
+ * Copyright (C) 2011 by LA7ECA, Øyvind Hanssen (ohanssen@acm.org)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,6 +18,8 @@ import java.util.*;
 import java.util.regex.*;
 import java.io.*;
 import se.raek.charset.*;
+import java.text.*;
+
 
 /**
  * Channel for sending/receiving APRS data. 
@@ -25,15 +27,21 @@ import se.raek.charset.*;
 public abstract class Channel
 {
      private static final long HRD_TIMEOUT = 1000 * 60 * 40; /* 40 minutes */
-     private LinkedHashMap<String, Date> _heard = new LinkedHashMap();
-   
+     protected LinkedHashMap<String, Heard> _heard = new LinkedHashMap();
+           
+     protected static class Heard {
+         public Date time; 
+         public String path;
+         public Heard(Date t, String p)
+           { time = t; path = p;}
+     }
      
      private void removeOldHeardEntries()
      {
-          Iterator<Date> it = _heard.values().iterator();
+          Iterator<Heard> it = _heard.values().iterator();
           Date now = new Date();
           while (it.hasNext()) {
-              Date x = it.next();
+              Date x = it.next().time;
               if (now.getTime() > x.getTime() + HRD_TIMEOUT)
                  it.remove();
               else
@@ -46,7 +54,7 @@ public abstract class Channel
      *  APRS packet.
      */ 
     public static class Packet implements Cloneable {
-        public String from, to, msgto, via, report; 
+        public String from, to, msgto, via, via_orig, report; 
         public char type; 
         public boolean thirdparty = false; 
         Channel source;
@@ -68,7 +76,11 @@ public abstract class Channel
     private static Pattern _ppat = Pattern.compile
        ("([\\w\\-]+)>([\\w\\-]+)(((,[\\w\\-]+\\*?))*):(.*)");
 
-
+    
+    private DateFormat df = new SimpleDateFormat("dd MMM yyyy HH:mm:ss",
+           new DateFormatSymbols(new Locale("no")));
+    
+    
     /* 
      * Perhaps we should use a list of receivers in a later version, but for now
      * two is sufficient: A parser and an igate.
@@ -81,6 +93,11 @@ public abstract class Channel
     public static final String _rx_encoding = "X-UTF-8_with_cp-850_fallback";
     public static final String _tx_encoding = "UTF-8";
 
+
+
+    public abstract String getShortDescr(); 
+    
+    
     /**
       * Returns true if call is heard.
       */
@@ -89,19 +106,67 @@ public abstract class Channel
          removeOldHeardEntries();
          return _heard.containsKey(call);
      }
-         
+           
+           
+           
+    /**
+     * Return last heard digipeater path for a callsign
+     */
+    public String heardPath(String call)
+     {
+         Heard x = _heard.get(call);
+         if (x==null)
+            return null;
+         else
+            return x.path;
+     }
+     
+     
+     
+    /**
+     * Generate a reverse path for a callsign based on last heard digi path.
+     */
+    public static String getReversePath(String path)
+    {
+       String result = "";
+       Stack<String> st = new Stack<String>();
+       String[] p = path.split(",");
+       boolean dflag = false;
+       for(String x : p) {       
+          if (x.length() < 1)
+             break;
+          if (x.charAt(x.length()-1) == '*') {
+             x = x.substring(0, x.length()-1);
+             dflag = true;
+          }
+          else if (dflag) 
+             break;
+          if (!x.matches("(WIDE|TRACE|NOR|SAR|TCPIP|TCPXX|NOGATE|RFONLY|NO_TX).*")) 
+             st.push(x);   
+       }
+       if (dflag) 
+          while (!st.empty())
+            result += (st.pop() + ",");
+       return result.length() == 0 ? "" : result.substring(0,result.length()-1);
+    }
+    
+    
+    
    /**
      * Number of stations heard.
      */     
     public int nHeard()
        { return _heard.keySet().size(); }
        
+       
     
     public PrintWriter getWriter()
        { return _out; }
 
+
     
     public abstract void sendPacket(Channel.Packet p);
+    
     
     
     /**
@@ -111,11 +176,12 @@ public abstract class Channel
     public void setReceivers(Receiver r1, Receiver r2)
        { _r1 = r1; _r2 = r2; }
            
+           
     
     /**
      * Convert text string to packet structure. 
      */
-    private Packet string2packet(String packet)
+    protected Packet string2packet(String packet)
     {
         Matcher m = _ppat.matcher(packet);
         if (m.matches())
@@ -145,27 +211,44 @@ public abstract class Channel
                 p.msgto = p.report.substring(1,9).trim();
 
             /* Remove first comma in path */
-            else if (p.via != null && p.via.length() > 0)
-               p.via = p.via.substring(1).trim();
+            p.via = p.via.trim();
+            while (p.via != null && p.via.length() > 0 && p.via.charAt(0) == ',')
+                  p.via = p.via.substring(1);
+
             return p;
         }
         return null;
     }
 
+
+
+    /**
+     * Register a station as heard (or not) based on its packet.
+     */
+    protected abstract void regHeard(Packet p);
+    
+           
+           
+           
     /**
      * Process incoming packet. 
      * To be called from subclass. Parses packet, updates heard table, checks for
      * duplicates and if all is ok, deliver packet to receivers.
      */
     protected void receivePacket(String packet, boolean dup)
-    {
+    {             
+       if (packet == null || packet.length() < 1)
+          return;
+       System.out.println(df.format(new Date()) + " ["+getShortDescr()+"] "+packet);    
        Packet p = string2packet(packet);
        if (p == null)
-          return;
+          return;  
        p.source = this;
-       _heard.put(p.from, new Date());
-
        dup = _dupCheck.checkPacket(p.from, p.to, p.report);
+       if (!dup) 
+          /* Register heard, only for first instance of packet, not duplicates */
+          regHeard(p);
+       
        if (_r1 != null) 
            _r1.receivePacket(p, dup);
        if (_r2 != null)
