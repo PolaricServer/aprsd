ab39bf97b2951e51b39d98b45de67d9abfd4ab94
oivindh <oivindh@f49ff3f0-531d-43b2-bf2a-0429c094f1ea>
2011-12-30 22:44:12 +0000

    Object in range test in gate-to-rf

--- src/Igate.java
+++ src/Igate.java
@@ -25,16 +25,22 @@ import uk.me.jstott.jcoord.*;
  */
 public class Igate implements Channel.Receiver
 { 
-    private Channel _inetChan, _rfChan;
-    private boolean _allowRf;
-    private String  _myCall; /* Move this ? */
-    private long    _msgcnt = 0; 
-    private String  _defaultPath, _alwaysRf;
+    private Channel   _inetChan, _rfChan;
+    private boolean   _allowRf, _gateObj;
+    private String    _myCall; /* Move this ? */
+    private long      _msgcnt = 0; 
+    private String    _defaultPath, _pathObj, _alwaysRf;
+    private int       _rangeObj;
+    private StationDB _db;    
         
         
-    public Igate(Properties config) 
+    public Igate(Properties config, StationDB db) 
     {
+        _db = db;
         _allowRf = config.getProperty("igate.rfgate.allow", "true").trim().matches("true|yes");
+        _gateObj = config.getProperty("igate.rfgate.objects", "false").trim().matches("true|yes");
+        _pathObj = config.getProperty("objects.rfgate.path", "").trim(); 
+        _rangeObj = Integer.parseInt(config.getProperty("objects.rfgate.range", "0").trim());
         _myCall = config.getProperty("igate.mycall", "").trim().toUpperCase();
         _defaultPath = config.getProperty("message.rfpath", "WIDE1-1").trim();
         _alwaysRf = config.getProperty("message.alwaysRf", "").trim();
@@ -80,30 +86,34 @@ public class Igate implements Channel.Receiver
      */
     private void gate_to_rf(Channel.Packet p)
     {
-       if (  /* Receiver heard on RF */
-              ( _rfChan.heard(p.to) || (p.msgto!= null && 
-                 (_rfChan.heard(p.msgto) || p.msgto.matches(_alwaysRf))))
-                
-            && /* Sender NOT heard on RF */
+       if ( ( (  /* Receiver heard on RF */
+                 ( _rfChan.heard(p.to) || (p.msgto!= null && 
+                    (_rfChan.heard(p.msgto) || p.msgto.matches(_alwaysRf))))
+            
+               && /* AND Receiver not heard on INET side */ 
+                 ( ! _inetChan.heard(p.to)  && !( p.msgto!=null && _inetChan.heard(p.msgto)))      
+            )
+            || /* OR object and in range */
+                ( _gateObj && p.type == ';' && object_in_range(p, _rangeObj) ) 
+          ) 
+          && /* AND Sender NOT heard on RF */
                ! _rfChan.heard(p.from)
-               
-            && /* Receiver not heard on INET side */ 
-               ( ! _inetChan.heard(p.to)  && !( p.msgto!=null && _inetChan.heard(p.msgto)))
-               
-            && /* No TCPXX, NOGATE, or RFONLY in header */
-               ! p.via.matches(".*((TCPXX)|NOGATE|RFONLY|NO_TX).*") ) 
+                      
+          && /* AND no TCPXX, NOGATE, or RFONLY in header */
+               ! p.via.matches(".*((TCPXX)|NOGATE|RFONLY|NO_TX).*") 
+       )    
        {        
           System.out.println("*** Gated to RF");
         
           /* Now, get a proper path for the packet. 
-           * If possible, a reverse of the path last heard from the recipient.
+           * For messages, if possible, a reverse of the path last heard from the recipient.
            */
           String path = _rfChan.heardPath(p.msgto); 
           p.via_orig = p.via;
-          if (path == null) 
-             // null means not heard at all
-             p.via = _defaultPath;
-          else
+          p.via = _defaultPath;
+          if (p.type == ';' && _pathObj != null)
+             p.via = _pathObj;
+          if (p.type == ':' && path != null) 
              p.via = Channel.getReversePath(path); 
                    
           _rfChan.sendPacket(p);
@@ -112,8 +122,22 @@ public class Igate implements Channel.Receiver
     
     
     
+    private boolean object_in_range(Channel.Packet p, int range)
+    {
+       /* We assume that object's position is already parsed and in database. */
+       if (Main.ownpos == null && Main.ownpos.getPosition() == null)
+            return true;
+     
+       AprsPoint obj = _db.getItem(p.msgto+'@'+p.from);
+       if (obj == null)
+            return false;
+       return (obj.distance(Main.ownpos) < range*1000);
+    }
+    
+    
+    
     /**
-     * Respond to query be sending a capabilities report. 
+     * Respond to query by sending a capabilities report. 
      * For now, we only respond to queries on the RF channel.
      */
     private void answer_query()
