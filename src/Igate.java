66f9eb4aa911fb4a3894f5a059a8c33e395d12c0
oivindh <oivindh@f49ff3f0-531d-43b2-bf2a-0429c094f1ea>
2012-05-02 20:42:55 +0000

    Filter out packets from self

--- src/Igate.java
+++ src/Igate.java
@@ -201,7 +201,7 @@ public class Igate implements Channel.Receiver, ManagedObject
      */
     public synchronized void receivePacket(Channel.Packet p, boolean dup)
     {
-        if (dup)
+        if (dup || _api.getOwnPos().getIdent().equals(p.msgto))
            return;
         if (p.source == _rfChan) {
            if (p.report.matches("\\?IGATE\\?.*"))
