97f33e38533be1b9bc8f210c14a394e7ed9105e5
oivindh <oivindh@f49ff3f0-531d-43b2-bf2a-0429c094f1ea>
2012-07-30 23:44:03 +0000

    Support GPS tracking

--- src/Trail.java
+++ src/Trail.java
@@ -94,6 +94,10 @@ public class Trail implements Iterable<Trail.Item>, Serializable
         _sum_speed += sp;
         boolean added = false;
          
+        if (length() > 0 && "(GPS)".equals(path) && Math.abs(crs - getFirst().course) < 20 &&
+                 t.getTime() < getFirst().time.getTime() + 30000)
+           return added;
+
         /* New report is newer than the last report - put it first */
         if ( _items.size() == 0 || 
             (t.getTime() >= getFirst().time.getTime() && getFirst().distance(p) > mindist)) {
