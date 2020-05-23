24207375cca6a9cdc4e5b0122a3ee6d3e1fd5f4d
ohanssen <ohanssen@acm.org>
2020-05-23 22:05:53 +0200

    Dev. version

--- src/aprsd/Main.java
+++ src/aprsd/Main.java
@@ -29,7 +29,7 @@ import java.util.concurrent.*;
 
 public class Main implements ServerAPI
 {
-   public  static String version = "2.5";
+   public  static String version = "2.5+";
    public static String toaddr  = "APPS25";
    
    private static StationDB db = null;
