f6c0df57d40a1e05f7965be3262172ebb0fed050
Øyvind Hanssen <ohanssen@acm.org>
2021-11-11 20:52:23 +0100

    Checkfilter

---
+++ src/aprsd/CheckFilter.java
@@ -0,0 +1,16 @@
+
+package no.polaric.aprsd;
+import no.polaric.aprsd.filter.*;
+
+
+public class CheckFilter
+{
+  
+  
+    public static void main(String[] args) {
+        var main = new Main();
+        main.simpleInit();
+        ViewFilter.init(main);
+    }
+  
+}
