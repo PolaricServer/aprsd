bfc7fbd7cecb6d828480c17724edf9963bcbd384
Øyvind Hanssen <ohanssen@acm.org>
2023-03-31 23:05:29 +0200

    Doc fixes

--- src/TrackerPoint.java
+++ src/TrackerPoint.java
@@ -1,6 +1,6 @@
  
 /* 
- * Copyright (C) 2016-20 by LA7ECA, Øyvind Hanssen (ohanssen@acm.org)
+ * Copyright (C) 2016-2023 by LA7ECA, Øyvind Hanssen (ohanssen@acm.org)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
