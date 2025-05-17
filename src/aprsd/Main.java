92068c7bf347623995fb2a0c412104f9726238ab
Øyvind Hanssen <ohanssen@acm.org>
2025-05-17 20:12:55 +0200

    Ready for release

--- src/aprsd/Main.java
+++ src/aprsd/Main.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2019-2023 by LA7ECA, Øyvind Hanssen (ohanssen@acm.org)
+ * Copyright (C) 2019-2025 by LA7ECA, Øyvind Hanssen (ohanssen@acm.org)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,8 +29,8 @@ import java.util.logging.*;
 
 public class Main implements ServerAPI
 {
-   public  static String version = "3.1+";
-   public  static String toaddr  = "APPS31";
+   public  static String version = "3.2";
+   public  static String toaddr  = "APPS32";
    
    private static StationDB db = null;
    private static AprsParser parser = null;
