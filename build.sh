8795e6296c9847af9ac69b5e25a888d26b271c1f
Øyvind Hanssen <ohanssen@acm.org>
2025-08-25 20:56:21 +0200

    Build files..

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd src/filter
+jflex filters.lex
+byaccj -Jpackage=no.polaric.aprsd.filter filters.y
+cd ../..
+mvn clean dependency:copy-dependencies -DoutputDirectory=lib -DincludeScope=runtime package 
