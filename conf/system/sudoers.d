7fd7e3e4fe794ce28d2e15ecd3770aebcc71faf9
Øyvind Hanssen <ohanssen@acm.org>
2025-09-03 21:50:35 +0200

    Installation: sudoers file

--- conf/system/sudoers.d
+++
@@ -1,4 +0,0 @@
-# Allow polaric user to set clock
-polaric	ALL = (ALL) NOPASSWD: /bin/date
-# Allow polaric user to restart server and to change user pw
-polaric	ALL = (ALL) NOPASSWD: /usr/bin/polaric-restart, /usr/bin/htpasswd *
